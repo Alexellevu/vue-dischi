23ef7620389bd9416f7d932e92c8f1a90fe8c352
alexellevu <newnapster83@gmail.com>
2021-05-28 17:00:57 +0200

    response

--- assets/main.js
+++ assets/main.js
@@ -2,7 +2,7 @@ const app = new Vue({
     el: '#app',
 
     data:{
-        response:''
+     
 
     },
 
@@ -15,7 +15,7 @@ const app = new Vue({
         axios
         .get('https://flynn.boolean.careers/exercises/api/array/music')
         .then(response => {
-            console.log(response.poster);
+            console.log(response);
         })
         
     
